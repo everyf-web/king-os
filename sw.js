7c65bc901904226ea83155d616b857711bfcd29d
everyf-web <sssergeytrade@gmail.com>
2026-08-23 14:36:52 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,45 +1,5 @@
-const CACHE="king-os-v1-3";
+const CACHE="king-os-v1-4";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-
-self.addEventListener("install",event=>{
-  event.waitUntil(
-    caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())
-  );
-});
-
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch",event=>{
-  const req=event.request;
-
-  if(req.mode==="navigate"){
-    event.respondWith(
-      fetch(req)
-        .then(res=>{
-          const copy=res.clone();
-          caches.open(CACHE).then(cache=>cache.put("./index.html",copy)).catch(()=>{});
-          return res;
-        })
-        .catch(()=>caches.match("./index.html").then(r=>r||caches.match("./")))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(req)
-      .then(res=>{
-        if(req.method==="GET" && res.ok){
-          const copy=res.clone();
-          caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{});
-        }
-        return res;
-      })
-      .catch(()=>caches.match(req))
-  );
-});
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",event=>{const req=event.request;if(req.mode==="navigate"){event.respondWith(fetch(req).then(res=>{const copy=res.clone();caches.open(CACHE).then(cache=>cache.put("./index.html",copy)).catch(()=>{});return res}).catch(()=>caches.match("./index.html").then(r=>r||caches.match("./"))));return}event.respondWith(fetch(req).then(res=>{if(req.method==="GET"&&res.ok){const copy=res.clone();caches.open(CACHE).then(cache=>cache.put(req,copy)).catch(()=>{})}return res}).catch(()=>caches.match(req))) });
